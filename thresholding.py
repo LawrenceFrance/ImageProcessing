5ed0bdafc47fe07ee1cec8bab0cda221daefa216
LawrenceFrance <46723332+LawrenceFrance@users.noreply.github.com>
2019-09-20 20:01:23 +0100

    Segmenting image

--- thresholding.py
+++ thresholding.py
@@ -44,8 +44,8 @@ denoise = cv.fastNlMeansDenoisingColored(original1, None,h,hColor,templateWindow
 # GAUSSIAN FILTERING
 gaus_blur = cv.GaussianBlur(grey,(9,9),0)
 
-pics.append(gaus_blur)
-names.append('Gaussian blur')
+# pics.append(gaus_blur)
+# names.append('Gaussian blur')
 
 # THRESHOLDING ###################################################################
 
@@ -80,9 +80,9 @@ grey_blurred = cv.cvtColor(blurred,cv.COLOR_BGR2GRAY)
 # OTSU THRESHOLDING
 
 ret_otsu1,otsu1 = cv.threshold(grey,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-pics.append(otsu1)
-names.append('Otsu')
-ret_otsu2,otsu2 = cv.threshold(gaus_blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# pics.append(otsu1)
+# names.append('Otsu')
+ret_otsu2,otsu2 = cv.threshold(grey_blurred,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 pics.append(otsu2) 
 names.append('Otsu blurred')
 
@@ -92,13 +92,34 @@ names.append('Otsu blurred')
 # use denoised image, morphological edge detector
 elKernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (13,13))
 gradient = cv.morphologyEx(denoise, cv.MORPH_GRADIENT, elKernel)
-pics.append(gradient)
-names.append('Edges')
+# pics.append(gradient)
+# names.append('Edges')
 
 # canny edge detector
 canny = cv.Canny(denoise,50,100)
-pics.append(canny)
-names.append('Canny')
+# pics.append(canny)
+# names.append('Canny')
+
+
+# FILLING REGION OF INTEREST
+
+# close operation
+closingKernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10,10))
+close = cv.morphologyEx(otsu2, cv.MORPH_CLOSE, closingKernel)
+
+pics.append(close)
+names.append('Closed')
+
+# erode 
+eroded = cv.erode(close, None, iterations=6)
+
+pics.append(eroded)
+names.append('Eroded')
+
+# find contours
+(cnting, contours, _) = cv.findContours(eroded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
 
 
 
